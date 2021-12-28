db5720b697c2d6fa334f9d60d95a659727128376
antiikdev <ilkka.a.kotilainen@gmail.com>
2021-12-28 08:33:32 +0800

    2nd ver

--- area-size.c
+++ area-size.c
@@ -1,32 +1,63 @@
 /**
- * @brief C-program to calculate area size
- * with different units
- * TODO: different units for the area size
- * TODO: functionality for forest industry purposes
+ * @brief C-program to calculate area size with different units
+ * @author antiikdev (www.antiik.dev)
+ * @version 28 December 2021
+ * TODO ideas: different unit conversions for the area size
+ * TODO ideas: functionality for forest calculations
  */
 
 #include <stdio.h>
 
 
 /**
- * @brief Print program instructions
+ * @brief Prints program instructions
  */
 void printInstructions() 
 {
-	printf("Program prints area size.\n");
+	printf("Program calculates area size. Enter 0 to exit.\n");
 }
 
 
 /**
- * @brief Method asks user input
+ * @brief Asks user input
  * @param l length
  * @param w width
  */
 void askInput(double *l, double *w)
 {
-	printf("Enter length and height (e.g. 1 2) >");
-	scanf("%lf %lf",&*l,&*w);
+	printf("Enter length in meters >");
+	scanf("%lf",&*l);
+	if ( *l == 0) return;
+	printf("Enter width in meters >");
+	scanf("%lf",&*w);
+	if ( *w == 0) return;
 	printf("\n");
+}
+
+
+/**
+ * @brief Asks user input if area size should be converted
+ * @return 1 if yes, 0 if no
+ */
+int askConvertResults()
+{
+	char answer;
+	printf("Do you want to convert the result into km^2? y or n >");
+	scanf(" %c", &answer);
+	printf("\n");
+	if ( answer == 'y' ) return 1;
+	return 0;
+}
+
+
+/**
+ * @brief Calculates area size in km^2
+ * @param r original area size avlue in m^2
+ * @return area size value in km^2
+ */
+double convertResult(double *r)
+{
+	return *r / 1000000.0;
 }
 
 
@@ -44,11 +75,21 @@ double calculateArea(double l, double w)
 
 /**
  * @brief Prints area size
- * @param r size of the area
+ * @param r size of the area in m^2
  */
 void print(double r)
 {
-	printf("Area size is %1.1lf \n", r);
+	printf("Area size is %1.1lf m^2.\n", r);
+}
+
+
+/**
+ * @brief Prints area size
+ * @param r size of the area in km^2
+ */
+void printKm(double r)
+{
+	printf("Area size is %1.1lf km^2.\n", r);
 }
 
 
@@ -58,11 +99,20 @@ void print(double r)
  */
 int main(void)
 {
-	double length, width, result;
+	double length=0, width=0, result=0;
+	int out;
 	
 	printInstructions();
 	askInput(&length, &width);
 	result = calculateArea(length, width);
 	print(result);
+	
+	// If convert to km^2
+	if ( result >= 1000000 ) out = askConvertResults();
+	if ( out == 1 )
+	{
+		result = convertResult(&result);
+		printKm(result);
+	}
 	return 0;
 }
